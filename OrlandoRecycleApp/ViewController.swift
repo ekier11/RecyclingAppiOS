bc474b2823b167cc34426dfdaa5a06b93c5756fd
Elizabeth Kiernan <eckiernan11@gmail.com>
2021-03-22 18:11:53 -0400

    Initial Commit

---
+++ OrlandoRecycleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrlandoRecycleApp
+//
+//  Created by Elizabeth Kiernan on 3/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
