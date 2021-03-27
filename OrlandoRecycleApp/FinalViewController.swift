f8653dbdb9fbb0e79ecc0cd8ccc49310ff78cd3c
ekier11 <ekiernan011@gmail.com>
2021-03-26 21:38:50 -0400

    initial commit

---
+++ OrlandoRecycleApp/FinalViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FinalViewController.swift
+//  OrlandoRecycleApp
+//
+//  Created by Elizabeth Kiernan on 3/24/21.
+//
+import UIKit
+
+class FinalViewController: UIViewController {
+    @IBOutlet weak var recyclableResultLabel: UILabel!
+    @IBOutlet weak var zeroWasteMessageLabel: UILabel!
+    @IBOutlet weak var homeButton: UIButton!
+    @IBOutlet weak var zeroWasteInfoButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+}
