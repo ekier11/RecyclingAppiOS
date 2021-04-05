2741ad7276827f8611768017c8d0aa7161155b18
ekier11 <ekiernan011@gmail.com>
2021-04-05 19:29:35 -0400

    Updated webpage links

--- OrlandoRecycleApp/ZeroWasteViewController.swift
+++ OrlandoRecycleApp/ZeroWasteViewController.swift
@@ -11,8 +11,12 @@ class ZeroWasteViewController: UIViewController {
 
     @IBOutlet weak var firstTextLabel: UILabel!
     @IBOutlet weak var secondTextLabel: UILabel!
-    @IBOutlet weak var commercialFoodWasteLabel: UILabel!
     @IBOutlet weak var commercialFoodWasteButton: UIButton!
+    @IBOutlet weak var recycleAtHomeButton: UIButton!
+    @IBOutlet weak var backyardCompostingButton: UIButton!
+    @IBOutlet weak var electronicWasteButton: UIButton!
+    @IBOutlet weak var returnCookingOilButton: UIButton!
+    @IBOutlet weak var foodWasteDropOffButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +26,37 @@ class ZeroWasteViewController: UIViewController {
         
         secondTextLabel.text = ("Orlando continues to strive to become a zero waste community, aiming to eliminate sending solid waste to landfills by 2040.")
         
-        commercialFoodWasteButton.addTarget(self, action: Selector(("didTapCFWButton")), for: .touchUpInside)
-
-        
     }
     
-    @IBAction func didTapCFWButton(sender: AnyObject) {
-        UIApplication.shared.openURL(URL(string: "https://www.orlando.gov/Trash-Recycling/Commercial-Food-Waste-Recycling")!)
+    @IBAction func didTapCommercialFoodWasteButton(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.orlando.gov/Trash-Recycling/Commercial-Food-Waste-Recycling")! as URL, options:
+            [:], completionHandler: nil)
     }
+    
+    @IBAction func didTapRecycleAtHomeButton(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.orlando.gov/Trash-Recycling/What-Goes-Where")! as URL, options:
+            [:], completionHandler: nil)
+    }
+    
+    @IBAction func didTapBackyardCompostingButton(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.orlando.gov/Trash-Recycling/Request-a-Free-Composter")! as URL, options:
+            [:], completionHandler: nil)
+    }
+    
+    @IBAction func didTapElectronicWasteButton(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.orlando.gov/Our-Government/Departments-Offices/Public-Works/Streets-and-Stormwater-Division/Keep-Orlando-Beautiful/Electronic-Waste-and-Textile-Recycling-Event")! as URL, options:
+            [:], completionHandler: nil)
+    }
+    
+    @IBAction func didTapReturnCookingOilButton(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.orlando.gov/Trash-Recycling/Return-Your-Cooking-Oil-Recycling-Container")! as URL, options:
+            [:], completionHandler: nil)
+    }
+    
+    @IBAction func didTapFoodWasteDropOffButton(_ sender: Any) {
+        UIApplication.shared.open(URL(string:"https://www.orlando.gov/Initiatives/Food-Waste-Drop-Off")! as URL, options:
+            [:], completionHandler: nil)
+    }
+    
     
 }
